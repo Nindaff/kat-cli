eb609a9baa57eafa7084bb06e76d0af6ba1c8071
Nicholas Pisacane <pisacanen@gmail.com>
2016-02-29 20:18:01 -0800

    initial

---
+++ test/lib/Query.test.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const should = require('should')
+const Query = require('../../lib/Query')
+
+const OPTS = [
+  [{}, 'https://kat.cr/new/'],
+  [{ query: 'test', field: 'age' }, 'https://kat.cr/usearch/test/?field=time_add&sorder=desc'],
+  [{ category: 'tv' }, 'https://kat.cr/tv/'],
+  [{ category: 'movies', query: 'test' }, 'https://kat.cr/usearch/test/'],
+  [{ query: 'test test' }, 'https://kat.cr/usearch/test%20test/']
+]
+
+describe('Query', function() {
+  it('options', () => {
+    OPTS.forEach((tuple) => {
+      new Query(tuple[0])._getURI().should.equal(tuple[1])
+    })
+  })
+
+  it('_paginate()', () => {
+    let query = new Query()
+    query._paginate(-1)
+    query._page.should.equal(0)
+
+    query._paginate(1)
+    query._page.should.equal(1)
+
+    query._paginate(-1)
+    query._page.should.equal(0)
+
+    query._paginate(42)
+    query._page.should.equal(1)
+    query._paginate(1)
+    query._page.should.equal(2)
+
+    query._paginate(-1)
+    query._page.should.equal(1)
+  })
+
+  it('lifecycle', () => {
+    let query = new Query({ query: 'test' })
+    return query.next()
+      .then((results) => {
+        results.length.should.equal(25)
+        let firstFirstName = results[0].name
+
+        return query.prev()
+          .then((results) => {
+            results[0].name.should.equal(firstFirstName)
+            return query.next()
+          })
+          .then((results) => {
+            results[0].name.should.not.equal(firstFirstName)
+            query.destroy()
+            should(query.results).equal(null)
+            should(query.options).equal(null)
+            query._alive.should.equal(false)
+            return query.next()
+          })
+          .then(() => {
+            throw new Error('Should be dead')
+          })
+          .catch((err) => {
+            err.message.should.equal('Query is dead')
+          })
+      })
+  })
+})
